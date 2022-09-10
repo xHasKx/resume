1cb927de5684785fbe50f978fb0c0420bd55b200
Alexander Kiranov <xhaskx@gmail.com>
2022-09-10 18:52:53 +0300

    partial implementation

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { parse } from 'yaml';
 import { readFile, writeFile } from 'fs/promises';
 import { renderFile } from 'ejs';
 
+function makeId(str) {
+	return str.toLowerCase().replace(/\s/g, '-').replace(/\W/g, '_');
+}
+
 async function main() {
 	// load CV data in YAML form
 	const cvYaml = await readFile('./data/cv.yaml', { encoding: 'utf-8' });
@@ -9,7 +13,25 @@ async function main() {
 	// parse data from YAML form into js object
 	const cv = parse(cvYaml);
 
-	// TODO: expand data
+	// extending data with cross-references between projects and programming languages
+	const programming_languages = {};
+	for (const lang of cv.programming_languages) {
+		programming_languages[lang.title] = lang;
+		lang.projects = [];
+		lang.id = `prog-lang-${makeId(lang.title)}`;
+	}
+	for (const proj of cv.projects) {
+		proj.id = `project-${makeId(proj.title)}`;
+		const languages = [];
+		for (const lname of proj.languages) {
+			const lang = programming_languages[lname];
+			if (!lang)
+				throw new Error(`no programming_language by title: ${lname}`);
+			lang.projects.push(proj);
+			languages.push(lang);
+		}
+		proj.languages = languages;
+	}
 
 	// render html
 	const html = await renderFile('./template/index.ejs', cv, {});
